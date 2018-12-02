8b3729153b53ef5816c6addc3e7943151a81dbc6
Anderson Coelho <anderson-tomas@hotmail.com>
2018-12-02 13:35:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
+def my_collect(array)
+  counter = 0
+  modded = []
+while counter < array.length
 
 
+  yield array[counter]
+  modded << array
+  
+  counter+=1
+end
+return modded
+
+binding.pry
+end
